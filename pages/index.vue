9f726b0f37d68db0e8176ab7e895d67efe6b0336
Yasumasa Ashida <ys.ashida@gmail.com>
2023-02-16 18:37:26 +0900

    refactor: add components/payjp_checkout

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,11 @@
 <template>
-  <div id="payjp_dialog"></div>
+  <PayjpCheckout :onCreatedHandler="onCreated" :onFailedHandler="onFailed" />
 </template>
 
 <script setup lang="ts">
-let script: HTMLElement | null | undefined = null
+  const onCreated = (payload: any) => {
+    console.log(payload.token)
+  }
 
-const runtimeConfig = useRuntimeConfig()
-
-const dataKey = runtimeConfig.payjpDataKey
-const className="payjp-button"
-
-onMounted( () => {
-  window.payjpCheckoutOnCreated = onCreateToken
-  window.payjpCheckoutOnFailed = onCreateTokenFailed
-  script = document.createElement('script')
-  script.setAttribute('src', 'https://checkout.pay.jp/')
-  script.setAttribute('data-on-created', 'payjpCheckoutOnCreated')
-  script.setAttribute('data-on-failed', 'payjpCheckoutOnFailed')
-  script.setAttribute('data-key', dataKey)
-  script.setAttribute('class', className)
-  script.setAttribute('data-partial', 'true')
-  document.querySelector('#payjp_dialog')?.appendChild(script)
-})
-onBeforeUnmount( () => {
-  document.querySelector('#payjp_dialog')?.removeChild(script as Node)
-  window.PayjpCheckout = null
-  window.payjpCheckoutOnCreated = null
-  window.payjpCheckoutOnFailed = null
-})
-
-const onCreateToken = (response: any) => {
-}
-
-const onCreateTokenFailed = (response: any) => {
-}
+  const onFailed = (payload: any) => {}
 </script>
