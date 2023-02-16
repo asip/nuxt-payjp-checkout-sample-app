0f4c5dd849b0a4a2999413e6e0a4a05a8597879c
Yasumasa Ashida <ys.ashida@gmail.com>
2023-02-16 15:30:16 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="payjp_dialog"></div>
+</template>
+
+<script setup lang="ts">
+let script: HTMLElement | null | undefined = null
+
+const runtimeConfig = useRuntimeConfig()
+
+const dataKey = runtimeConfig.payjpDataKey
+const className="payjp-button"
+
+onMounted( () => {
+  window.payjpCheckoutOnCreated = onCreateToken
+  window.payjpCheckoutOnFailed = onCreateTokenFailed
+  script = document.createElement('script')
+  script.setAttribute('src', 'https://checkout.pay.jp/')
+  script.setAttribute('data-on-created', 'payjpCheckoutOnCreated')
+  script.setAttribute('data-on-failed', 'payjpCheckoutOnFailed')
+  script.setAttribute('data-key', dataKey)
+  script.setAttribute('class', className)
+  script.setAttribute('data-partial', 'true')
+  document.querySelector('#payjp_dialog')?.appendChild(script)
+})
+onBeforeUnmount( () => {
+  document.querySelector('#payjp_dialog')?.removeChild(script as Node)
+  window.PayjpCheckout = null
+  window.payjpCheckoutOnCreated = null
+  window.payjpCheckoutOnFailed = null
+})
+
+const onCreateToken = (response: any) => {
+}
+
+const onCreateTokenFailed = (response: any) => {
+}
+</script>
