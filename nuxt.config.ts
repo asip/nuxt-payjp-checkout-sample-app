f62800eef803c06022f65246c9f90f9f94a97c8a
Yasumasa Ashida <ys.ashida@gmail.com>
2025-04-26 00:23:03 +0900

    chore: update nuxt to 3.16.2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
+
   runtimeConfig: {
     public: {
       payjpDataKey: '',
     }
-  }
+  },
+
+  compatibilityDate: "2025-04-26"
 })
